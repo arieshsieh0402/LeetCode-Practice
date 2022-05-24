36818b1a32340572f9b62f8ea306ec18ea53ae5d
Aries Hsieh <westwood0402@gmail.com>
2022-05-24 23:49:32 +0800

    modify medium 33

--- 33_search_in_rotated_sorted_array.py
+++ 33_search_in_rotated_sorted_array.py
@@ -2,6 +2,9 @@ from typing import List
 
 
 def search(nums: List[int], target: int) -> int:
+    """
+    33. Search in Rotated Sorted Array
+    """
     left = 0
     right = len(nums) - 1
 
@@ -9,11 +12,19 @@ def search(nums: List[int], target: int) -> int:
         mid = (left + right) // 2
         if target == nums[mid]:
             return mid
+        # The array can be divided into the left array and the right array
+        # from the flip point.
+
+        # If mid > right, means the hard sorted array may be the left array.
+        # So if the target is between left and right,
+        # It can find the target by Binary Search.
+        # If not, we can find the target in right array.
         elif nums[mid] > nums[right]:
             if nums[left] <= target < nums[mid]:
                 right = mid - 1
             else:
                 left = mid + 1
+        # The following code is the opposite way of the above code.
         else:
             if nums[mid] < target <= nums[right]:
                 left = mid + 1
