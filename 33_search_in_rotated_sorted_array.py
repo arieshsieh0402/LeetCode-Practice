619901f3073f2bf10a8df28ccbd333c35200f904
Aries Hsieh <westwood0402@gmail.com>
2022-05-24 23:29:57 +0800

    medium 33

--- 33_search_in_rotated_sorted_array.py
+++ 33_search_in_rotated_sorted_array.py
@@ -9,16 +9,21 @@ def search(nums: List[int], target: int) -> int:
         mid = (left + right) // 2
         if target == nums[mid]:
             return mid
-        if nums[left] < nums[mid]:
+        elif nums[mid] > nums[right]:
             if nums[left] <= target < nums[mid]:
                 right = mid - 1
             else:
                 left = mid + 1
         else:
-            if nums[right] > nums[mid]:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
 
     return -1
+
+# URL:https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+
+# ===========Time Complexity(Worst Case Performence)=========== #
+
+# O(logn)
