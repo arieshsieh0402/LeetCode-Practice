ae6b77d7174817628d30e69eb94445a5739854c6
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-06-29 22:05:44 +0800

    easy 2000

--- 2000_reverse_prefix_of_word.py
+++ 2000_reverse_prefix_of_word.py
@@ -1,23 +1,23 @@
 def reverse_prefix(word: str, ch: str) -> str:
-    """
-    Solution of 2000-Reverse-Prefix-of-Word.
-
-    word -- consists of lowercase English letters.
-    ch -- is a lowercase English letter.
-
-    """
-    # Reverse the prefix of word segment
-    # by using string slice method with indexing syntax.
     for key, value in enumerate(word):
         if value == ch:
             return word[:key + 1][::-1] + word[key + 1:]
-    # If not found the “ch”, return the word.
     return word
 
-# URL:https://leetcode.com/problems/reverse-prefix-of-word/
 
-# ===========Time Complexity(Worst Case Performence)=========== #
+def reverse_prefix_two_pointers(word: str, ch: str) -> str:
+    word_list = list(word)
+    left = 0
+    right = 0
 
-# If the index of "ch" is at the last of word or not found,
-# the for-loop will iterate the length of word times (n).
-# In conclusion, the time complexity in worst case is O(n).
+    for i in range(len(word_list)):
+        if word_list[i] == ch:
+            right = i
+            break
+
+    while left <= right:
+        word_list[left], word_list[right] = word_list[right], word_list[left]
+        left += 1
+        right -= 1
+
+    return "".join(word_list)
