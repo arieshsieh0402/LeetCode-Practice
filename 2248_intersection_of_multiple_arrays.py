b2046882dc69e0ed7c99f924111bece450c8dd05
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-03 12:08:30 +0800

    easy 2248 new sol

--- 2248_intersection_of_multiple_arrays.py
+++ 2248_intersection_of_multiple_arrays.py
@@ -1,17 +1,26 @@
 from typing import List
 
 
-def intersection(nums: List[List[int]]) -> List[int]:
-    """
-    Solution of 2248. Intersection of Multiple Arrays
-    """
+def intersection_set_fun(nums: List[List[int]]) -> List[int]:
     answer_set = set(nums[0])
+
     for i in range(1, len(nums)):
         answer_set = answer_set.intersection(set(nums[i]))
     return sorted(answer_set)
 
-# URL: https://leetcode.com/problems/intersection-of-multiple-arrays/
 
-# ===========Time Complexity(Worst Case Performence)=========== #
+def intersection_hash_table(nums: List[List[int]]) -> List[int]:
+    count = {}
 
-# O(n)
+    flatted_nums = sum(nums, [])
+
+    for num in flatted_nums:
+        count[num] = count.get(num, 0) + 1
+
+    target_num_count = len(nums)
+    result = []
+
+    for key, value in count.items():
+        if value == target_num_count:
+            result.append(key)
+    return sorted(result)
