7df5ca91b27ae770de8293a28b42d404f8408662
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-05 22:48:14 +0800

    easy 100

---
+++ 100_same_tree.py
@@ -0,0 +1,9 @@
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if not p and not q:
+        return True
+    elif not p or not q:
+        return False
+    elif p.val != q.val:
+        return False
+    else:
+        return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
