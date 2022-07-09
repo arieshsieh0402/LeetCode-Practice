8e9d35cb85819c9ed191907a6a106f4c09511e67
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-09 21:44:22 +0800

    easy 145

--- 145_binary_tree_postorder_traversal.py
+++ 145_binary_tree_postorder_traversal.py
@@ -5,12 +5,16 @@
 #         self.left = left
 #         self.right = right
 
+
+# =====PostOrder=====
+
+
 # Recursion
-def postorder_traversal(root: Optional[TreeNode]) -> List[int]:
+def post_order_traversal(root: Optional[TreeNode]) -> List[int]:
     traversal_post_order = []
 
     def dfs_post_order(node: Optional[TreeNode]) -> None:
-        nonlocal traversal_post_order
+        nonlocal traversal_postorder
         if not node:
             return
 
@@ -27,7 +31,7 @@ def postorder_traversal(root: Optional[TreeNode]) -> List[int]:
 # Iteration
 from collections import deque
 
-def postorder_traversal(root: Optional[TreeNode]) -> List[int]:
+def post_order_traversal(root: Optional[TreeNode]) -> List[int]:
     traversal_post_order = deque()
     stack = [root]
 
@@ -39,7 +43,90 @@ def postorder_traversal(root: Optional[TreeNode]) -> List[int]:
                 stack.append(node.left)
             if node.right:
                 stack.append(node.right)
+    return list(traversal_post_order)
+
+
+# Iteration without deque
+def postorder_traversal(root: Optional[TreeNode]) -> List[int]:
+    traversal_post_order = []
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+        if node:
+            traversal_post_order.appen(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
     return traversal_post_order[::-1]
 
-    
-        
+
+# =====PreOrder=====
+
+# Recursion
+def pre_order_traversal(root: Optional[TreeNode]) -> List[int]:
+    traversal_pre_order = []
+
+    def dfs_pre_order(node: Optional[TreeNode]) -> None:
+        nonlocal traversal_pre_order
+        if not node:
+            return
+        traversal_pre_order.append(node.val)
+        dfs_pre_order(node.left)
+        dfs_pre_order(node.right)
+
+    dfs_pre_order(root)
+
+    return traversal_pre_order
+
+# Iteration
+def pre_order_traversal(root: Optional[TreeNode]) -> List[int]:
+    traversal_pre_order = []
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+        if node:
+            traversal_pre_order.append(node.val)
+            if node.right:
+                stack.append(node.right) # Coz stack LIFO 
+            if node.left:
+                stack.append(node.left)
+    return traversal_pre_order
+
+
+# =====InOrder=====
+
+# Recursion
+def in_order_traversal(root: Optional[TreeNode]) -> List[int]:
+    traversal_in_order = []
+
+    def dfs_in_order(node: Optional[TreeNode]) -> None:
+        nonlocal traversal_in_order
+        if not node:
+            return
+        dfs_in_order(node.left)
+				traversal_in_order.append(node.val)
+        dfs_in_order(node.right)
+
+    dfs_in_order(root)
+
+    return traversal_in_order
+
+
+# Iteration
+def in_order_traversal(root: Optional[TreeNode]) -> List[TreeNode]:
+    traversal_in_order = []
+    stack = []
+
+    while stack or node:
+        if node:
+            stack.append(root)
+            root = root.left
+        else:
+            node = stack.pop()
+            traversal_in_order.append(node.val)
+            node = node.right
+
+    return traversal_in_order
