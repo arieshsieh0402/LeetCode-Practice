c30f2cddc38fc600daa618198cce76833306ea05
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-11 11:32:47 +0800

    easy 206

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -1,74 +1,79 @@
-class Node:
+# class Node:
 
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-
-    def __init__(self):
-        self.head = None
-        self.num_of_nodes = 0
-
-    def insert_start(self, data):
-        self.num_of_nodes += 1
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-
-    def insert_end(self, data):
-        self.num_of_nodes += 1
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-        else:
-            current_node = self.head
-            while current_node.next is not None:
-                current_node = current_node.next
-
-            current_node.next = new_node
-
-    def reverse_list(self):
-        prev = None
-        current = self.head
-        while current is not None:
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
-        self.head = prev
-
-    def printList(self):
-        show_on_array = []
-        temp = self.head
-        while temp:
-            show_on_array.append(temp.data)
-            temp = temp.next
-        return show_on_array
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
 
-lk = LinkedList()
+# class LinkedList:
 
-for i in range(1, 6):
-    lk.insert_end(i)
+#     def __init__(self):
+#         self.head = None
+#         self.num_of_nodes = 0
 
-print(f"This is original linked list: {lk.printList()}")
+#     def insert_start(self, data):
+#         self.num_of_nodes += 1
+#         new_node = Node(data)
+
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             new_node.next = self.head
+#             self.head = new_node
+
+#     def insert_end(self, data):
+#         self.num_of_nodes += 1
+#         new_node = Node(data)
+
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             current_node = self.head
+#             while current_node.next is not None:
+#                 current_node = current_node.next
+
+#             current_node.next = new_node
+
+#     def printList(self):
+#         show_on_array = []
+#         temp = self.head
+#         while temp:
+#             show_on_array.append(temp.data)
+#             temp = temp.next
+#         return show_on_array
 
 
-lk.reverse_list()
-print(f"This is reversed linked list: {lk.printList()}")
+# lk = LinkedList()
+
+# for i in range(1, 6):
+#     lk.insert_end(i)
+
+# print(f"This is original linked list: {lk.printList()}")
 
 
-# URL:https://leetcode.com/problems/reverse-linked-list/
+# lk.reverse_list()
+# print(f"This is reversed linked list: {lk.printList()}")
 
-# Time Complexity
-# O(n)
 
-# Space
-# O(n) ---> not sure
+# Iteratively
+def reverse_list_iter(head: Optional[ListNode]) -> Optional[ListNode]:
+    prev = None
+    current = head
+    while current is not None:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    return prev
+
+
+# Recursively
+def reverse_list_recur(self, head):
+    return self._reverse(head)
+
+    def reverse(self, node, prev=None):
+        if not node:
+            return prev
+        next_node = node.next
+        node.next = prev
+        return self._reverse(next_node, node)
