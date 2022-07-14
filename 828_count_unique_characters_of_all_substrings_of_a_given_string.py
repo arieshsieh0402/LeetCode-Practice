1b87c52f844854bbadac7d265be30a1ce8ae8a47
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-14 16:37:58 +0800

    hard 828 add sol

--- 828_count_unique_characters_of_all_substrings_of_a_given_string.py
+++ 828_count_unique_characters_of_all_substrings_of_a_given_string.py
@@ -8,13 +8,36 @@ def unique_letter_string_brute(s: str) -> int:
         return len([key for key, val in unique_chars_dict.items() if val == 1])
 
     unique_char_dict = {}
-    length_count = 0
+    result = 0
 
     for i in range(len(s)):
         for j in range(i, len(s)):
             sub_string = s[i:j + 1]
             if sub_string not in unique_char_dict:
                 unique_char_dict[sub_string] = count_unique_chars(sub_string)
-            length_count += unique_char_dict[sub_string]
+            result += unique_char_dict[sub_string]
 
-    return length_count
+    return result
+
+
+# Expansion
+
+def unique_letter_string_expansion(s: str) -> int:
+    result = 0
+
+    for index, char in enumerate(s):
+        left = index - 1
+        right = index + 1
+
+        while left >= 0 and s[left] != char:
+            left -= 1
+
+        while right < len(s) and s[right] != char:
+            right += 1
+
+        left_length = index - left
+        right_length = right - index
+
+        result += left_length * right_length
+
+    return result
