2ac3b3398d30461485e39df088201334e5ad3f2c
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-07-17 09:59:31 +0800

    hard 2262new sol

--- 2262_total_appeal_of_a_string.py
+++ 2262_total_appeal_of_a_string.py
@@ -1,5 +1,8 @@
+from string import ascii_lowercase
+
+
 # Brute-Force
-def appeal_sum(s: str) -> int:
+def appeal_sum_brute(s: str) -> int:
     appeal_char_dict = {}
     result = 0
 
@@ -9,5 +12,33 @@ def appeal_sum(s: str) -> int:
             if sub_string not in appeal_char_dict:
                 appeal_char_dict[sub_string] = len(set((sub_string)))
             result += appeal_char_dict[sub_string]
+
+    return result
+
+
+# Brute-Force Improvement
+def appeal_sum_brute_imp(s: str) -> int:
+    def check_char_appeal(sub_string):
+        appeal_char_dict = {char: False for char in ascii_lowercase}
+        char_remain = 26
+        
+        for char in sub_string:
+            if not appeal_char_dict[char]:
+                appeal_char_dict[char] = True
+                char_remain -= 1
+            if char_remain == 0:
+                break
+            
+        return len([key for key, val in appeal_char_dict.items() if val])
+
+    result = 0
+    sub_char_dict = {}
+
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            sub_string = s[i:j + 1]
+            if sub_string not in sub_char_dict:
+                sub_char_dict[sub_string] = check_char_appeal(sub_string)
+            result += sub_char_dict[sub_string]
 
     return result
--- 828_count_unique_characters_of_all_substrings_of_a_given_string.py
+++ 828_count_unique_characters_of_all_substrings_of_a_given_string.py
@@ -1,24 +1,24 @@
-# Brute-Force
 from collections import Counter
 from string import ascii_uppercase
 
 
+# Brute-Force
 def unique_letter_string_brute(s: str) -> int:
     def count_unique_chars(s: str) -> int:
         unique_chars_dict = Counter(s)
         return len([key for key, val in unique_chars_dict.items() if val == 1])
 
-    unique_char_dict = {}
-    result = 0
+    sub_char_dict = {}
+    length_count = 0
 
     for i in range(len(s)):
         for j in range(i, len(s)):
             sub_string = s[i:j + 1]
-            if sub_string not in unique_char_dict:
-                unique_char_dict[sub_string] = count_unique_chars(sub_string)
-            result += unique_char_dict[sub_string]
+            if sub_string not in sub_char_dict:
+                sub_char_dict[sub_string] = count_unique_chars(sub_string)
+            length_count += sub_char_dict[sub_string]
 
-    return result
+    return length_count
 
 
 # Expansion
