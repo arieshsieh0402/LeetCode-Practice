57a9cc852c3801501fb12318b5c188993163e6dd
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-13 23:26:32 +0800

    844

---
+++ string/844_backspace_string_compare.py
@@ -0,0 +1,26 @@
+def backspace_compare(s: str, t: str) -> bool:
+    stack_s, stack_t = [], []
+
+    for char in s:
+        if not stack_s and char != '#':
+            stack_s.append(char)
+        elif stack_s and char == '#':
+            stack_s.pop()
+        elif char != '#':
+            stack_s.append(char)
+
+    for char in t:
+        if not stack_t and char != '#':
+            stack_t.append(char)
+        elif stack_t and char == '#':
+            stack_t.pop()
+        elif char != '#':
+            stack_t.append(char)
+
+    return True if stack_s == stack_t else False
+
+# Time: O(n)
+# Space: O(n)
+
+def backspace_compare_improve(s: str, t: str) -> bool:
+    
