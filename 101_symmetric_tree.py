6153b21b706e31c2a002d5790545aa684b925c7b
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-28 23:07:50 +0800

    17

--- 101_symmetric_tree.py
+++ 101_symmetric_tree.py
@@ -3,14 +3,14 @@ def is_symmetric(root: Optional[TreeNode]) -> bool:
     if root == None:
             return True
         return is_symmetric_child_node(root.left, root.right)
-    
+
 def is_symmetric_child_node(left: [TreeNode], right: [TreeNode]) -> bool:
     if not left and not right:
         return True
     if not left or not right:
         return False
-    return ((left.val == right.val) and 
-    (is_symmetric_child_node(left.left, right.right)) and 
+    return ((left.val == right.val) and
+    (is_symmetric_child_node(left.left, right.right)) and
     (is_symmetric_child_node(left.right, right.left)))
 
 
---
+++ 17_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,27 @@
+def letter_combinations(digits: str):
+    if not digits:
+        return []
+
+    digit_map = {
+        '1': '',
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+        '0': ' '
+    }
+
+    all_combinations = ['']
+
+    for digit in digits:
+        curr_combinations = []
+        for letter in digit_map[digit]:
+            for combination in all_combinations:
+                curr_combinations.append(combination + letter)
+        all_combinations = curr_combinations
+
+    return all_combinations
