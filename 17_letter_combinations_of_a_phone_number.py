0c69dcb7bc313e69769ac8f3b3100d9042425729
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-28 23:56:31 +0800

    17

--- 17_letter_combinations_of_a_phone_number.py
+++ 17_letter_combinations_of_a_phone_number.py
@@ -23,3 +23,35 @@ def letter_combinations(digits: str):
         all_combinations = curr_combinations
 
     return all_combinations
+
+
+def letter_combinations_dfs(digits: str):
+    if not digits:
+        return []
+
+    table = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+
+    ans = []
+
+    def backtrack(i, curr):
+        if len(curr) == len(digits):
+            ans.append(''.join(list(curr)))
+            return
+        else:
+            for val in table[digits[i]]:
+                curr.append(val)
+                backtrack(i+1, curr)
+                curr.pop()
+
+    backtrack(0, [])
+
+    return ans
