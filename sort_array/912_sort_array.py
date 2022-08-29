db0bf36134d1eeeb255cee82564ccf740d15cc4a
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-29 13:14:11 +0800

    912

---
+++ sort_array/912_sort_array.py
@@ -0,0 +1,55 @@
+def bubble_sort(nums):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+    return nums
+
+
+def selection_sort(nums):
+    for i in range(len(nums) - 1):
+        min_index = i
+        for j in range(i, len(nums)):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[min_index], nums[i] = nums[i], nums[min_index]
+
+    return nums
+
+
+def merge_sort(nums):
+    def merge(arr1, arr2):
+        i = 0
+        j = 0
+        result = []
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] > arr2[j]:
+                result.append(arr2[j])
+                j += 1
+            else:
+                result.append(arr1[i])
+                i += 1
+
+        # arr1 or arr2 might have some elements left
+        # use loop to put them all into result
+
+        while i < len(arr1):
+            result.append(arr1[i])
+            i += 1
+
+        while j < len(arr2):
+            result.append(arr2[j])
+            j += 1
+
+        return result
+
+    if len(nums) == 1:
+        return nums
+    else:
+        middle = len(nums) // 2
+        left_arr = nums[:middle]
+        right_arr = nums[middle:len(nums)]
+        return merge(merge_sort(left_arr), merge_sort(right_arr))
