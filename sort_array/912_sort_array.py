ad3ed259f8d69341768444fd5f78036b782e4a72
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-08-31 23:29:01 +0800

    912

--- sort_array/912_sort_array.py
+++ sort_array/912_sort_array.py
@@ -54,3 +54,23 @@ def merge_sort(nums):
         right_arr = nums[middle:len(nums)]
         return merge(merge_sort(left_arr), merge_sort(right_arr))
 
+
+def partition(nums, p, r):
+    pivot = nums[r]
+    i = p - 1
+    for j in range(p, r):
+        if nums[j] <= pivot:
+            i += 1
+            nums[j], nums[i] = nums[i], nums[j]
+    nums[i + 1], nums[r] = nums[r], nums[i + 1]
+
+    return i + 1
+
+
+def quick_sort(nums, p, r):
+    if p < r:
+        q = partition(nums, p, r)
+        quick_sort(nums, p, q - 1)
+        quick_sort(nums, q + 1, r)
+
+# quick_sort(nums, 0, len(nums) - 1)
