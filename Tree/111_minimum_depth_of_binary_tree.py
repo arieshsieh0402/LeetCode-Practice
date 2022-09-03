0367b1a6e20b7b1e9174b00d3a38a5cb58f88aed
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-03 22:23:55 +0800

    fix wrong

--- Tree/111_minimum_depth_of_binary_tree.py
+++ Tree/111_minimum_depth_of_binary_tree.py
@@ -1,15 +1,32 @@
-class Solution:
-    # DFS
-    def min_depth_dfs(self, root) -> int:
-        if not root:
-            return 0
-        if not root.left or not root.right:
-            return max(
-                self.min_depth_dfs(root.left),
-                self.min_depth_dfs(root.right)
-            ) + 1
-        else:
-            return min(
-                self.min_depth_dfs(root.left),
-                self.min_depth_dfs(root.right)
-            ) + 1
+from collections import deque
+
+
+def min_depth_dfs(self, root) -> int:
+    if not root:
+        return 0
+    if not root.left or not root.right:
+        return max(
+            self.min_depth_dfs(root.left),
+            self.min_depth_dfs(root.right)
+        ) + 1
+    else:
+        return min(
+            self.min_depth_dfs(root.left),
+            self.min_depth_dfs(root.right)
+        ) + 1
+
+
+def min_depth_bfs(root):
+    if not root:
+        return 0
+
+    q = deque([(root, 1)])
+
+    while q:
+        curr_node, depth = q.popleft()
+        if not curr_node.right and not curr_node.left:
+            return depth
+        if curr_node.left:
+            q.append((curr_node.left, depth + 1))
+        if curr_node.right:
+            q.append((curr_node.right, depth + 1))
