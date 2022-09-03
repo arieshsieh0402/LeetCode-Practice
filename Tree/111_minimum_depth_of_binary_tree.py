819e88e94986485714d90b10e07b0dbca2088817
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-03 22:24:50 +0800

    Modify 111

--- Tree/111_minimum_depth_of_binary_tree.py
+++ Tree/111_minimum_depth_of_binary_tree.py
@@ -1,18 +1,18 @@
 from collections import deque
 
 
-def min_depth_dfs(self, root) -> int:
+def min_depth_dfs(root):
     if not root:
         return 0
     if not root.left or not root.right:
         return max(
-            self.min_depth_dfs(root.left),
-            self.min_depth_dfs(root.right)
+            min_depth_dfs(root.left),
+            min_depth_dfs(root.right)
         ) + 1
     else:
         return min(
-            self.min_depth_dfs(root.left),
-            self.min_depth_dfs(root.right)
+            min_depth_dfs(root.left),
+            min_depth_dfs(root.right)
         ) + 1
 
 
