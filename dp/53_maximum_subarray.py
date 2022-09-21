c1d3e85ef6ac40e9e9258e572b1463538d84c089
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-21 11:06:44 +0800

    53

--- dp/53_maximum_subarray.py
+++ dp/53_maximum_subarray.py
@@ -1,8 +1,3 @@
-# Given an integer array nums, find the contiguous sub array
-# (containing at least one number)
-# which has the largest sum and return its sum.
-# A subarray is a contiguous part of an array.
-
 from typing import List
 
 
@@ -12,17 +7,6 @@ def max_sub_array(nums: List[int]) -> int:
     max_sum = nums[0]
     cur_sum = 0
     for num in nums:
-        # Compare the current sum with the current number,
-        # if the current number is larger,
-        # it means that the current sum will not be the best subarray.
         cur_sum = max(cur_sum + num, num)
         max_sum = max(max_sum, cur_sum)
     return max_sum
-
-# URL:https://leetcode.com/problems/maximum-subarray/
-
-# Time Complexity
-# O(n)
-
-# Space
-# O(1)
