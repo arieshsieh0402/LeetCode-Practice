15133b09f5ec4d39f4260cbe877eef69cc44c420
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-24 17:33:45 +0800

    198

--- dp/198_house_robber.py
+++ dp/198_house_robber.py
@@ -1,20 +1,7 @@
 from typing import List
 
 
-# Solution 1
-# Recursion
-
-# Find the base case
-# Assume the function to find max looted values is rob(i).
-# When you face the house_(i), you have two choice, rob or not.
-# If you rob, this means that the house you grabbed last time won't be
-# house_(i - 1) , because you can't grab an adjacent house.
-# In this case,
-# the value you looted until house_(i) is nums[i] plus rob(i - 2).
-# Otherwise, if you don't rob the house_(i), that means the the value
-# you have looted will be rob(i - 1).
-
-def rob(nums: List[int]) -> int:
+def rob_brute_force(nums: List[int]) -> int:
     if not nums:
         return 0
     elif len(nums) == 1:
@@ -22,17 +9,12 @@ def rob(nums: List[int]) -> int:
     elif len(nums) == 2:
         return max(nums[0], nums[1])
     i = len(nums) - 1
-    return max(nums[i] + rob(nums[:i - 1]), rob(nums[:i]))
+    return max(
+        nums[i] + rob_brute_force(nums[:i - 1]), rob_brute_force(nums[:i])
+    )
 
-# Time Complexity: O(?)
-# Space Complexity: O(?)
 
-
-# Solution 2
-# Dynamic Programming
-# Use a dynamic programming table dp to store the looted values
-
-def rob(nums: List[int]) -> int:
+def rob_dp(nums: List[int]) -> int:
     if not nums:
         return 0
     elif len(nums) == 1:
@@ -45,6 +27,3 @@ def rob(nums: List[int]) -> int:
     for i in range(2, len(nums)):
         dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
     return dp[-1]
-
-# Time Complexity: O(n)
-# Space Complexity: O(n)
