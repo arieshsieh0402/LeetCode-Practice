90174ffdb9127e0030b1b32e2879221839b23d60
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-25 14:14:33 +0800

    322

--- dp/322_coin_change.py
+++ dp/322_coin_change.py
@@ -1,13 +1,14 @@
+import math
 from typing import List
 
 
 def coin_change(coins: List[int], amount: int) -> int:
-    dp = [0] + [float('inf') for _ in range(amount)]
+    dp = [0] + [math.inf for _ in range(amount)]
     for coin in coins:
         for i in range(coin, amount + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    if dp[-1] == float('inf'):
+    if dp[-1] == math.inf:
         return -1
     else:
         return dp[-1]
