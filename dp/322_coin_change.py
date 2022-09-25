666261ce21bb32ff1f32c8c9fcd464f4c40a3be5
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-09-25 15:22:19 +0800

    322

--- dp/322_coin_change.py
+++ dp/322_coin_change.py
@@ -1,8 +1,9 @@
+from collections import deque
 import math
 from typing import List
 
 
-def coin_change(coins: List[int], amount: int) -> int:
+def coin_change_dp(coins: List[int], amount: int) -> int:
     dp = [0] + [math.inf for _ in range(amount)]
     for coin in coins:
         for i in range(coin, amount + 1):
@@ -12,3 +13,27 @@ def coin_change(coins: List[int], amount: int) -> int:
         return -1
     else:
         return dp[-1]
+
+
+def coin_change_bfs(coins: List[int], amount: int) -> int:
+    if amount == 0:
+        return 0
+
+    q = deque()
+    q.append(amount)
+    visited = set()
+    depth = 0
+    while q:
+        for _ in range(len(q)):
+            curr_amount = q.popleft()
+
+            if curr_amount < 0:
+                continue
+            elif curr_amount == 0:
+                return depth
+            if curr_amount not in visited:
+                visited.add(curr_amount)
+                for coin in coins:
+                    q.append(curr_amount - coin)
+        depth += 1
+    return -1
