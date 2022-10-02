015881d26ea6805e28abbf4a73d6eb30f3fcbf1e
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-10-02 22:29:00 +0800

    1800

--- 1800_maximum_ascending_subarray_sum.py
+++ 1800_maximum_ascending_subarray_sum.py
@@ -1,41 +1,17 @@
 def max_ascending_sum(nums: list[int]) -> int:
-    """
-    Solution of 1800. Maximum Ascending Subarray Sum.
-
-    nums -- an array of positive integers nums.
-
-    """
-    max_sum = nums[0]
-    left = 0
-    right = 1
-    while right <= len(nums) - 1:
-        if nums[right] > nums[right - 1]:
-            temp_sum = sum(nums[left:right + 1])
-            if temp_sum > max_sum:
-                max_sum = temp_sum
-            right += 1
+    if len(nums) == 1:
+        return nums[0]
+    max_sum = 0
+    head = 0
+    for i in range(1, len(nums)):
+        if nums[i] <= nums[i - 1]:
+            curr_sum = sum(nums[head:i])
+            max_sum = max(max_sum, curr_sum)
+            head = i
         else:
-            left = right
-            right += 1
+            if i == len(nums) - 1:
+                curr_sum = sum(nums[head:])
+                max_sum = max(max_sum, curr_sum)
+            else:
+                continue
     return max_sum
-
-    # for right in range(1, len(nums)):
-    #     if nums[i] <= nums[i - 1]:
-    #         temp_sum = sum(nums[pointer:i])
-    #         if temp_sum > max_sum:
-    #             max_sum = temp_sum
-    #         pointer = i
-    #     else:
-    #         if i == len(nums) - 1:
-    #             temp_sum = sum(nums[pointer:])
-    #             if temp_sum > max_sum:
-    #                 max_sum = temp_sum
-    #         else:
-    #             continue
-
-    return max_sum
-
-# URL:https://leetcode.com/problems/maximum-ascending-subarray-sum/
-
-# ===========Time Complexity(Worst Case Performence)=========== #
-# O(n)
