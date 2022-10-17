93e6253c688b851b57f98b70396feb30ae312dc7
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-10-17 20:11:14 +0800

    Modify 1800

--- 1800_maximum_ascending_subarray_sum.py
+++ 1800_maximum_ascending_subarray_sum.py
@@ -1,4 +1,4 @@
-def max_ascending_sum(nums: list[int]) -> int:
+def max_ascending_sum_1(nums):
     if len(nums) == 1:
         return nums[0]
     max_sum = 0
@@ -15,3 +15,39 @@ def max_ascending_sum(nums: list[int]) -> int:
             else:
                 continue
     return max_sum
+
+
+def max_ascending_sum_2(nums):
+    if len(nums) == 1:
+        return nums[0]
+    curr_sum = max_sum = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i-1]:
+            curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
+        else:
+            curr_sum = nums[i]
+    return max_sum
+
+
+def max_ascending_sum_3(nums):
+    '''
+    Return list version
+    '''
+
+    if len(nums) == 1:
+        return nums[0]
+
+    curr_sum = max_sum = nums[0]
+    head = 0
+    end = None
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i-1]:
+            curr_sum += nums[i]
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+                end = i + 1
+        else:
+            curr_sum = nums[i]
+            head = i
+    return nums[head:end]
