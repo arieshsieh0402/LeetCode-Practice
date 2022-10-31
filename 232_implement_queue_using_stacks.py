05eec8ea19e8b9548d25dc50697d4a84d817df30
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-10-31 23:34:53 +0800

    232

--- 232_implement_queue_using_stacks.py
+++ 232_implement_queue_using_stacks.py
@@ -39,3 +39,25 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+
+class MyQueueVerTwo:
+    def __init__(self):
+        self.stack = []
+        self.temp_stack = []
+
+    def push(self, x: int) -> None:
+        for _ in range(len(self.stack)):
+            self.temp_stack.append(self.stack.pop())
+        self.stack.append(x)
+        for _ in range(len(self.temp_stack)):
+            self.stack.append(self.temp_stack.pop())
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def peek(self) -> int:
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
