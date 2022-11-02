3b6076b6d0d680b132b589abb0cba9b08470b74b
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2022-11-02 16:07:38 +0800

    2, 26

--- Two_Pointers/1_two_sum/1_hash_map.py
+++ Two_Pointers/1_two_sum/1_hash_map.py
@@ -3,10 +3,8 @@ from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     nums_dt = {}
-
     for i, num in enumerate(nums):
         complement = target - num
-        if complement not in nums_dt:
-            nums_dt[complement] = i
-        else:
+        if complement in nums_dt:
             return [nums_dt[complement], i]
+        nums_dt[num] = i
