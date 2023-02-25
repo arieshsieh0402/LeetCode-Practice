a6e292a1a4fdc45a8de40d29d9bdcc6f16d6daa1
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2023-02-25 15:28:54 +0800

    Modify 26.

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -1,7 +1,43 @@
-def removeDuplicates(nums: List[int]) -> int:
-    idx = 1
+from typing import List
+
+
+def remove_duplicates_1(nums: List[int]) -> int:
+    next_distinct = 1
     for i in range(1, len(nums)):
         if nums[i] != nums[i - 1]:
-            nums[idx] = nums[i]
-            idx += 1
-    return idx
+            nums[next_distinct] = nums[i]
+            next_distinct += 1
+
+    return next_distinct
+
+
+def remove_duplicates_2(nums: List[int]) -> int:
+    duplicates = 0
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            duplicates += 1
+        else:
+            nums[i - duplicates] = nums[i]
+
+    return len(nums) - duplicates
+
+
+def remove_duplicates_3(nums: List[int]) -> int:
+    n = len(nums)
+    if n <= 2:
+        if n == 2 and nums[0] == nums[1]:
+            return 1
+        else:
+            return n
+
+    curr = nums[0]
+    left = right = 1
+
+    while right < n:
+        if nums[right] != curr:
+            curr = nums[right]
+            nums[right], nums[left] = nums[left], nums[right]
+            left += 1
+        right += 1
+
+    return left
