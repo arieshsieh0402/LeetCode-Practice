9b7b3b39ffb2215f3288c8ad5afec859cd55dcb7
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2023-02-28 12:32:11 +0800

    rename 907

--- 907_sum_subarray_mins.py
+++ 907_sum_subarray_mins.py
@@ -1,7 +1,16 @@
 from typing import List
 
 
-def sum_subarray_mins(arr: List[int]) -> int:
+def sum_subarray_mins_brute(arr: List[int]) -> int:
+    result = 0
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            sub_arr = arr[i:j + 1]
+            result += min(sub_arr)
+    return result % (10 ** 9 + 7)
+
+
+def sum_subarray_mins_expansion(arr: List[int]) -> int:
     result = 0
     for i, num in enumerate(arr):
         left = i - 1
