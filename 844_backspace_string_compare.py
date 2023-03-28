3e30c5893e157bb3ee9e96047e25252b139762c7
Hsieh-Te Hsieh <89285959+arieshsieh0402@users.noreply.github.com>
2023-03-28 23:12:46 +0800

    finished 844

--- 844_backspace_string_compare.py
+++ 844_backspace_string_compare.py
@@ -1,25 +1,47 @@
-def backspace_compare(s: str, t: str) -> bool:
-    stack_s, stack_t = [], []
+def backspace_compare_stack(s: str, t: str) -> bool:
+    stack_s, stack_c = [], []
 
-    for char in s:
-        if not stack_s and char != '#':
-            stack_s.append(char)
-        elif stack_s and char == '#':
+    for c in s:
+        if stack_s and c == '#':
             stack_s.pop()
-        elif char != '#':
-            stack_s.append(char)
+        elif c != '#':
+            stack_s.append(c)
 
-    for char in t:
-        if not stack_t and char != '#':
-            stack_t.append(char)
-        elif stack_t and char == '#':
-            stack_t.pop()
-        elif char != '#':
-            stack_t.append(char)
+    for c in t:
+        if stack_c and c == '#':
+            stack_c.pop()
+        elif c != '#':
+            stack_c.append(c)
 
-    return True if stack_s == stack_t else False
+    return stack_s == stack_c
 
-# Time: O(n)
-# Space: O(n)
 
-# def backspace_compare_improve(s: str, t: str) -> bool:
+def backspace_compare_two_pointer(s: str, t: str) -> bool:
+    i, j = len(s) - 1, len(t) - 1
+    s_skip, t_skip = 0, 0
+    while i >= 0 or j >= 0:
+        while i >= 0:
+            if s[i] == '#':
+                s_skip += 1
+                i -= 1
+            elif s_skip > 0:
+                s_skip -= 1
+                i -= 1
+            else:
+                break
+        while j >= 0:
+            if t[j] == '#':
+                t_skip += 1
+                j -= 1
+            elif t_skip > 0:
+                t_skip -= 1
+                j -= 1
+            else:
+                break
+        if i >= 0 and j >= 0 and s[i] != t[j]:
+            return False
+        if (i >= 0) != (j >= 0):
+            return False
+        i -= 1
+        j -= 1
+    return True
